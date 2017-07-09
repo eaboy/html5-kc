7fa3bf9ff7da17cf01180c0790740f08d74b7f47
Eduardo <e.aboy@sinteac.es>
2017-07-09 13:28:27 +0200

    Add tasks.html and update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,15 +1,79 @@
-var XHR;
+var tasks = [];
 
-if (window.XMLHttpRequest) {
-    XHR = new XMLHttpRequest();
-    XHR.open('GET', 'http://127.0.0.1:8000/api/tasks/1', true);
-    XHR.setRequestHeader('Content-Type', 'application/json');
+var drawTasks = function() {
+	$('#taskContainer').empty();
+
+	if (tasks.length == 0) {
+		$('#taskContainer').append("<li>No hay tareas pendientes</li>");
+	} else {
+		var contentToAdd = '';
+		for (var i = 0; i < tasks.length; i++) {
+			contentToAdd += '<li class="task-item">' + tasks[i].name + '<button class="deleteTask" data-task-id="' + tasks[i].id + '">Eliminar</button></li>'
+		}
+		$('#taskContainer').append(contentToAdd);
+	}
+}
+
+var getTasks = function () {
+    var XHR = new XMLHttpRequest();
+    XHR.open("GET", "http://localhost:8000/api/tasks", true);
+    XHR.setRequestHeader("Content-Type", "application/json");
+
     XHR.onreadystatechange = function () {
-        if (XHR.readyState === 4 && XHR.status === 200) {
-            console.log(JSON.parse(XHR.responseText));
+        if (XHR.readyState === 4) {
+            tasks = JSON.parse(XHR.responseText);
+            drawTasks();
         } else if (XHR.readyState === 4 && XHR.status === 404) {
-            console.log('Página no encontrada');
+            console.log("Página no encontrada");
         }
     }
-    XHR.send(JSON.stringify({"name":"Python serverJavaScript mola"}));
+
+    XHR.send();
 }
+
+var createTask = function (name) {
+    var XHR = new XMLHttpRequest();
+    XHR.open("POST", "http://localhost:8000/api/tasks", true);
+    XHR.setRequestHeader("Content-Type", "application/json");
+
+    XHR.onreadystatechange = function () {
+        if (XHR.readyState === 4) {
+            tasks.push(JSON.parse(XHR.responseText));
+            drawTasks();
+        } else if (XHR.readyState === 4 && XHR.status === 404) {
+            console.log("Página no encontrada");
+        }
+    }
+
+    XHR.send(JSON.stringify({"name": name}));
+}
+
+var deleteTask = function (id) {
+    var XHR = new XMLHttpRequest();
+    XHR.open("DELETE", "http://localhost:8000/api/tasks/" + id, true);
+    XHR.setRequestHeader("Content-Type", "application/json");
+
+    XHR.onreadystatechange = function () {
+        if (XHR.readyState === 4) {
+            console.log("tasks deleted");
+            getTasks();
+        } else if (XHR.readyState === 4 && XHR.status === 404) {
+            console.log("Página no encontrada");
+        }
+    }
+
+    XHR.send();
+}
+
+document.getElementById("sendNewTask").addEventListener("click", function (event) {
+    event.preventDefault();
+    createTask(document.getElementById("newTaskName").value);
+})
+
+
+getTasks();
+
+$(document).on('click', '.deleteTask', function(){
+	var id = $(this).data('taskId');
+	deleteTask(id);
+});
